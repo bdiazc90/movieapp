e111b65c9c88859247972fa6b956ccdc24fd818b
bdiazc90 <bdiazc90@gmail.com>
2022-08-05 23:46:17 -0500

    Reto 1: config fixed, favorite by user_id

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const api = {
+	url: "https://www.omdbapi.com",
+	key: "e50b7182",
+};
+
+export const authUsers = [
+	{
+		id: "1001",
+		username: "admin",
+		name: "Admin",
+		pass: "1234",
+	},
+	{
+		id: "1002",
+		username: "bruno",
+		name: "Bruno Díaz",
+		pass: "1234",
+	},
+	{
+		id: "1003",
+		username: "linder",
+		name: "Linder Hassinger",
+		pass: "1234",
+	},
+];
+
+const Config = {
+	authUsers,
+	api,
+};
+
+export default Config;
